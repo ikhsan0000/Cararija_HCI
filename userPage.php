8a3dd8dba20be10ab5f3a1634ff598331ea0c72d
Ikhsan Firdauz <ikhsan.firdauz@ui.ac.id>
2020-11-15 11:32:37 +0700

    dev build

--- userPage.php
+++ userPage.php
@@ -7,9 +7,8 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="CSS/mystyle.css">
-    <link rel="stylesheet" type="text/css" href="CSS/startbootstrap-simple-sidebar.css">
+    <link rel="stylesheet" type="text/css" href="CSS/sidebar.css">
     <link rel="stylesheet" type="text/css" href="CSS/card.css">
-
 
     <!-- Scrollbar Custom CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
@@ -24,6 +23,7 @@
 
       <!-- JQ -->
       <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 
 </head>
@@ -35,18 +35,25 @@
     
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item active mr-5">
+          <li class="nav-item mr-5">
             <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
           </li>
 
           <li class="nav-item mr-5">
             <a class="nav-link" href="#">LINK</a>
           </li>
-
-          <button type="button" class="btn my-btn-login mr-5 btn-size" id="login-btn">LOGIN</button>
-
-          <button type="button" class="btn btn-primary btn-size" id="register-btn">REGISTER</button>
           
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle pr-5" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Welcome, Admin
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#"><span><i class="fas fa-cog"></i> Preferences</span></a>
+                    <a class="dropdown-item" href="#"><span><i class="far fa-envelope"></i> Contact Support</span></a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="index.html"><span><i class="fas fa-power-off"></i> Log Out</span></a>
+                </div>
+            </li>
         </ul>
       </div>
       
@@ -117,9 +124,9 @@
     </button>
 
     <div class="row">
-    <?php for($i = 0; $i<5 ; $i++): ?>
+    <?php for($i = 0; $i<7 ; $i++): ?>
         <div class="col-sm-4">
-            <div class="card mt-5">
+            <div class="card mt-4 mb-3">
                 <img class="profile mt-4" src="assets/icon_perusahaan/google.png">
                 <div class="card-head">
                     <p class="card-title">Google</p>
@@ -163,12 +170,11 @@
             </div>
         </div>
         <?php endfor; ?>
+        
     </div>
-
+    <p style="font-size: 15px; item-align: center; text-align: center">Dst . . .</p>
   </div>
     
-
-
     <script type="text/javascript">
         $(document).ready(function () {
             $("#sidebar").mCustomScrollbar({
@@ -180,7 +186,10 @@
                 $('.collapse.in').toggleClass('in');
                 $('a[aria-expanded=true]').attr('aria-expanded', 'false');
             });
+
+
         });
+
     </script>
 </body>
 </html>
